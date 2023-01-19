7f14c15d20db1b1e6a5d236aff25320e74766a98
MaksMBO <maksbredyk@gmail.com>
2023-01-19 02:04:33 +0200

    fixed a problem with jerky search animation when reloading the page

--- static/js/search.js
+++ static/js/search.js
@@ -6,12 +6,11 @@ function searchShow(){
     var inp = document.getElementById("search-button-inp");
     if (document.getElementById("search-button-inp").style.visibility != "hidden") {
         inp.style.top = '-100%';
-        // document.getElementById("search-button-inp").style.transition = "top .5s cubic-bezier(0, 0, 1, 1), visibility 1s 1s"
         document.getElementById("search-button-inp").style.visibility = "hidden";
     }
     else {
         inp.style.top = '0';
-        // document.getElementById("search-button-inp").style.transition = "top .5s cubic-bezier(0, 0, 1, 1), visibility 1s 1s"
+        document.getElementById("search-button-inp").style.transition = "top .3s cubic-bezier(0, 0, 1, 1), visibility .3s"
         document.getElementById("search-button-inp").style.visibility = "inherit";
     }
 }
@@ -19,7 +18,6 @@ function searchShow(){
 function searchHide(){
     var inp = document.getElementById("search-button-inp");
     inp.style.top = '-100%';
-    // document.getElementById("search-button-inp").style.transition = "top .5s cubic-bezier(0, 0, 1, 1), visibility 1s 1s"
     document.getElementById("search-button-inp").style.visibility = "hidden";
 }
 
