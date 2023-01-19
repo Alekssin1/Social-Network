f6fe4ecbc78b10167dc2fbb94f20d98c440df933
MaksMBO <maksbredyk@gmail.com>
2023-01-19 02:01:40 +0200

    improved search menu appearance animation

--- static/js/search.js
+++ static/js/search.js
@@ -2,14 +2,16 @@ $(document).ready(function(){
     searchHide()
 });
 
-function searchShow(){
+function searchShow(){ 
     var inp = document.getElementById("search-button-inp");
     if (document.getElementById("search-button-inp").style.visibility != "hidden") {
         inp.style.top = '-100%';
+        // document.getElementById("search-button-inp").style.transition = "top .5s cubic-bezier(0, 0, 1, 1), visibility 1s 1s"
         document.getElementById("search-button-inp").style.visibility = "hidden";
     }
     else {
         inp.style.top = '0';
+        // document.getElementById("search-button-inp").style.transition = "top .5s cubic-bezier(0, 0, 1, 1), visibility 1s 1s"
         document.getElementById("search-button-inp").style.visibility = "inherit";
     }
 }
@@ -17,6 +19,7 @@ function searchShow(){
 function searchHide(){
     var inp = document.getElementById("search-button-inp");
     inp.style.top = '-100%';
+    // document.getElementById("search-button-inp").style.transition = "top .5s cubic-bezier(0, 0, 1, 1), visibility 1s 1s"
     document.getElementById("search-button-inp").style.visibility = "hidden";
 }
 
