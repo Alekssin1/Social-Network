b33f3199e6f36c86906f3237c1bb33eb6ce6ea4f
MaksMBO <maksbredyk@gmail.com>
2023-01-19 18:10:41 +0200

    added animation of follow and edit

--- static/js/showHide_follow.js
+++ static/js/showHide_follow.js
@@ -1,41 +1,65 @@
 $(document).ready(function(){   
-    FollowingHide();
-    FollowedHide();
-    EditHide();
+    FollowingHide(false);
+    FollowedHide(false);
+    EditHide(false);
 });
 
 function FollowingShow(){
+    var inp = document.getElementById("user_form");
     $("#user_form-back").show();
-    $("#user_form").show();
+    inp.style.opacity = '1';
+    document.getElementById("user_form").style.transition = "opacity .3s cubic-bezier(0, 0, 1, 1), visibility .3s";
+    document.getElementById("user_form").style.visibility = "inherit";
     $("body").css("overflow","hidden"); 
 }
 
-function FollowingHide(){
+function FollowingHide(second=true){
+    var inp = document.getElementById("user_form");
     $("#user_form-back").hide();
-    $("#user_form").hide();
+    inp.style.opacity = '0';
+    if (second) {
+       document.getElementById("user_form").style.transition = "opacity .3s cubic-bezier(0, 0, 1, 1), visibility .3s"; 
+    }
+    document.getElementById("user_form").style.visibility = "hidden";
     $("body").css("overflow","auto");
 }
 
 function FollowedShow(){
+    var inp = document.getElementById("userFollower_form");
     $("#userFollower_form-back").show();
-    $("#userFollower_form").show();
+    inp.style.opacity = '1';
+    document.getElementById("userFollower_form").style.transition = "opacity .3s cubic-bezier(0, 0, 1, 1), visibility .3s";
+    document.getElementById("userFollower_form").style.visibility = "inherit";
     $("body").css("overflow","hidden"); 
 }
 
-function FollowedHide(){
+function FollowedHide(second=true){
+    var inp = document.getElementById("userFollower_form");
     $("#userFollower_form-back").hide();
-    $("#userFollower_form").hide();
+    inp.style.opacity = '0';
+    if (second) {
+        document.getElementById("userFollower_form").style.transition = "opacity .3s cubic-bezier(0, 0, 1, 1), visibility .3s";
+    }
+    document.getElementById("userFollower_form").style.visibility = "hidden";
     $("body").css("overflow","auto");
 }
 
 function EditShow(){
+    var inp = document.getElementById("user_form_edit");
     $("#user_form_edit-back").show();
-    $("#user_form_edit").show();
+    inp.style.opacity = '1';
+    document.getElementById("user_form_edit").style.transition = "opacity .3s cubic-bezier(0, 0, 1, 1), visibility .3s";
+    document.getElementById("user_form_edit").style.visibility = "inherit";
     $("body").css("overflow","hidden"); 
 }
 
-function EditHide(){
+function EditHide(second=true){
+    var inp = document.getElementById("user_form_edit");
     $("#user_form_edit-back").hide();
-    $("#user_form_edit").hide();
+    inp.style.opacity = '0';
+    if (second) {
+        document.getElementById("user_form_edit").style.transition = "opacity .3s cubic-bezier(0, 0, 1, 1), visibility .3s";
+    }
+    document.getElementById("user_form_edit").style.visibility = "hidden";
     $("body").css("overflow","auto");
 }
