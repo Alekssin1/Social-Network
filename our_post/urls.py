d558d6fa28640573bba29a96fc56ef74b0d30a92
MaksMBO <maksbredyk@gmail.com>
2023-01-22 18:53:13 +0200

    delete post class

--- our_post/urls.py
+++ our_post/urls.py
@@ -3,8 +3,8 @@ from . import views
 urlpatterns = [
     path('', views.Post.as_view(), name='our_post'),
     path('like/<int:id>', views.Like_post.as_view(), name='like_post'),
-    path('comments/<post_id>', views.comments, name='comments'), 
+    path('comments/<post_id>', views.Comments.as_view(), name='comments'), 
     path('delete/comment/<int:id_comment>/', views.del_comment, name='delete_comment'),
-    path('delete/<int:id_post>/', views.del_post, name='delete'),
+    path('delete/<int:id_post>/', views.Delete_post.as_view(), name='delete'),
 ]
 
--- our_post/views.py
+++ our_post/views.py
@@ -10,6 +10,9 @@ from django.urls import reverse
 from users.views import profile
 from django.views import View
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 User = get_user_model()
 
@@ -39,12 +42,31 @@ class Like_post(DetailView):
             instance.likes.remove(request.user)
             instance.save() 
             return render( request, 'our_post/partials/like.html', context={'post':instance})
-
-@login_required(login_url="/profile/login/")
-def comments(request, post_id):
-    return render(request, 'our_post\post.html', 
-        context={'post': UserPost.objects.filter(id=int(post_id)).prefetch_related('comments').select_related('userId').first(),
-            "users": User.objects.all().select_related('avatar')})
+        
+class Comments(LoginRequiredMixin, DetailView):
+    model = UserPost
+    template_name = 'our_post\post.html'
+    pk_url_kwarg = 'post_id'
+    context_object_name = 'post'
+    login_url = reverse_lazy('login')
+    
+    def get_object(self, queryset=None):
+        return UserPost.objects.filter(id=int(self.kwargs.get("post_id"))).prefetch_related('comments').select_related('userId').first()
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['users'] = User.objects.all().select_related('avatar')
+        return context
+    
+class Delete_post(DeleteView):
+    model = UserPost
+    pk_url_kwarg = 'id_post'
+    
+    def get(self, request, *args, **kwargs):
+        user = UserPost.objects.get(id=self.kwargs.get('id_post'))
+        UserPost.objects.filter(id=self.kwargs.get('id_post')).delete()
+        return HttpResponseRedirect(reverse(profile, args=[user.userId.username]))
+    
 
 def del_post(request, id_post):
     user = UserPost.objects.get(id=id_post)
